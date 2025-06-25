9db29285b796d13aa2426d7b86ff738d0961cb10
Yann Martin D'Escrienne <yann.martin@doriane.com>
2025-06-25 12:37:22 +0200

    add label

--- src/Node.tsx
+++ src/Node.tsx
@@ -1,5 +1,6 @@
 import { Vector3 } from "three";
 import { Box } from "./Box";
+import { Text } from "@react-three/drei";
 
 interface INode {
 	id: string;
@@ -8,14 +9,29 @@ interface INode {
 	children: INode[];
 }
 
-export function Node({ current, deep, sibling }: { current: INode; deep: number, sibling:number }) {
-
-
+export function Node({
+	current,
+	deep,
+	sibling,
+}: {
+	current: INode;
+	deep: number;
+	sibling: number;
+}) {
 	return (
-		<group position={new Vector3(3,sibling*1.5,0)}>
-			<Box onSelect={() => {}} selected={true} />
+		<group position={new Vector3(3, sibling * 1.5 * deep, 0)}>
+			<Box onSelect={() => {}} selected={false} />
+			<Text
+				position={[0, 1, 1]}
+				fontSize={0.3}
+				color="white"
+				anchorX="center"
+				anchorY="bottom"
+			>
+				{current.id}
+			</Text>
 			{current.children.map((child: INode, i) => (
-				<Node current={child} deep={deep + 1} sibling={i}/>
+				<Node current={child} deep={deep + 1} sibling={i} />
 			))}
 		</group>
 	);
